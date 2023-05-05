a7c95c2c5eee51279646e3e339f9be57f9a243b4
Phill Conrad <pconrad@ucsb.edu>
2023-05-05 17:02:44 +0000

    pc - add admin.sh script and fix bug in db.sh

---
+++ admin.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+dokku config:set --no-restart ${1} ADMIN_EMAILS=phtcon@ucsb.edu,avishekde@ucsb.edu,vivianross@ucsb.edu,bzamoraflores@ucsb.edu,andrewpeng@ucsb.edu,rbriggs@ucsb.edu
--- db.sh
+++ db.sh
@@ -3,13 +3,22 @@
 dokku postgres:create ${1}-db
 dokku postgres:link ${1}-db ${1}
 
-RESULT=`dokku config:show proj-happycows | grep DATABASE_URL`
+RESULT=`dokku config:show proj-happycows | egrep "^DATABASE_URL"`
 PASSWORD=`echo "$RESULT" |  awk -F[:@] '{print $4}'`
 DATABASE=`echo "$RESULT" |  awk -F[/] '{print $4}'`
+
+
+echo "RESULT=$RESULT"
+echo "PASSWORD=$PASSWORD"
+echo "DATABASE=$DATABASE"
+
 
 IP=`dokku postgres:info ${1}-db | grep "Internal ip:"`
 IP=`echo ${IP/Internal ip: /} | tr -d '[:space:]'`
 URL="jdbc:postgresql://${IP}:5432/${DATABASE}"
+
+echo "IP=$IP"
+echo "URL=$URL"
 
 dokku config:set --no-restart ${1} JDBC_DATABASE_URL=${URL}
 dokku config:set --no-restart ${1} JDBC_DATABASE_USERNAME=postgres
